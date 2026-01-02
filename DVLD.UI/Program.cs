af44d29801e1f6b2a5d860b6096510bebb826647
Neo <nnmwtn@gmail.com>
2026-01-02 11:53:10 +0100

    Add files via upload

---
+++ DVLD.UI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using DVLD.UI.Layout;
+using DVLD.UI.Screens.User;
+using DVLD.UI.Helpers.Session;
+
+namespace DVLD.UI
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            clsAppSession.LoadCurrentUser();
+
+            if (clsAppSession.CurrentUser == null || !clsAppSession.CurrentUser.IsLogin || !clsAppSession.CurrentUser.IsRemembered)
+            {
+                Application.Run(new frmLogin());
+            }
+            else
+            {
+                Application.Run(new MainForm());
+            }
+        }
+    }
+}
